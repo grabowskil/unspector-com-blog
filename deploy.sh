59c9baa7645562a2cf568bc7a575bd6d6a117c89
grabowskil <lennartgrabowski@googlemail.com>
2021-06-03 16:17:03 +0200

    readded deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Create commit message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+# Build the project.
+echo ""
+echo ""
+echo "Committing changes to $(pwd)"
+hugo -D
+
+# Go To Public folder
+cd public
+
+if [ -z "$(git status --procelain)" ]; then
+    echo "nothing to commit"
+	exit 0
+else
+    # Add 'public' (Github Pages repo) changes to git and commit/push.
+	echo ""
+	echo ""
+	echo "Committing changes to $(pwd)"
+	git add .
+	git commit -m "$msg"
+	git push origin main
+
+	# Add this repos changes to git and commit/push. First 'cd' out of public
+	cd ..
+	echo ""
+	echo ""
+	echo "Committing changes to $(pwd)"
+	git add .
+	git commit -m "$msg"
+	git push origin master
+fi
+
+exit 0
